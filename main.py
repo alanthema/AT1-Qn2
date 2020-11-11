e59f6b3e46189d0f12fa96b4dfee2f1520323a51
alanthema <alan.ma94@gmail.com>
2020-11-11 06:42:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Author:
+#Date:
+#Version: 1.0
+#
+#Rock Paper Scissors Game
+#Two Players
+
+print("Welocme to Rock, Paper, Scissors!")
+print("Let's Begin...")
+name1 = input("Player 1: What's your name?")
+name2 = input("Player 2 : What's your name?\n")
+
+print("Hello " + name1 + "and " = name2)=
+print(name2 + " Close your eyes!")
+
+choice1 = input(name1 +  ":enter 'r' for rock, 'p' for  paper, and 's' for scissors: ")
+print("Great choice!" Now - cover your answer and ask "+name2 + " To choose. \n\n\n")
+choice2 = input(name2 +  ":enter 'r' for rock, 'p' for  paper, and 's' for scissors: ")
+
+if???
+
+print("Thanks for palying Rock, Paper, Scissors")
